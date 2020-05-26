d710d6b7dc7615d53f9475d2b2a3d00ec9afeae8
SzymonCzajka <szymonczajka4@gmail.com>
2020-05-26 15:44:09 +0200

    Create initial models and migrate database

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
-db_user = os.environ.get('DB_USERNAME', default='notes')
-db_password = os.environ.get('DB_PASSWORD', default="")
+db_user = os.environ.get('DB_USERNAME', default='demo')
+db_password = os.environ.get('DB_PASSWORD', default='password')
 db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
